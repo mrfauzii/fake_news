c9d64618a9ecb529c60ff245ea6fd126b8ce2986
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2026-05-31 17:33:03 +0700

    knowledge base fix

--- ai-api/controllers/kb_controller.py
+++ ai-api/controllers/kb_controller.py
@@ -5,11 +5,21 @@ from services.chroma_service import insert_to_chroma
 import requests
 
 def update_knowledge_base_controller(model, collection, data, batch_size=32,):
+    token =data.get("token")
+    url = "http://127.0.0.1:8000/scraper/done/" + token
+    
     print("🚀 Memulai proses scraping data hoaks...")
     latest_title = get_latest_title()
     print(f"📌 Judul terbaru di database: '{latest_title}'")
     df = scrape_new_hoaxes(latest_title)
     df = scrape_all(df)
+    if ((len(df) == 0) & (df)):
+        print("✅ knowledge base sudah paling baru")
+        requests.get(url)
+        return {
+                    "status": "success",
+                    "message": "knowledge base sudah paling baru"
+                }
     df = retry_scrape_nan(df)
     df = clean_dataframe(df)
     print(df.columns)
@@ -20,9 +30,6 @@ def update_knowledge_base_controller(model, collection, data, batch_size=32,):
 
     list_id = insert_to_mysql(df)
     insert_to_chroma(df, list_id, model, collection, batch_size)
-    
-    token =data.get("token")
-    url = "http://127.0.0.1:8000/scraper/done/" + token
     
     requests.get(url)
 
--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (env('APP_PUBLIC') === 'https') {
             URL::forceScheme('https');
         }
     }

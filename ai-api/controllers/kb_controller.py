d8a50b68b12fc18bcd9e9dff785bd086544095e8
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2026-05-31 13:09:13 +0700

    restore controller

--- ai-api/controllers/kb_controller.py
+++ ai-api/controllers/kb_controller.py
@@ -5,21 +5,21 @@ from services.chroma_service import insert_to_chroma
 import requests
 
 def update_knowledge_base_controller(model, collection, data, batch_size=32,):
-    # print("🚀 Memulai proses scraping data hoaks...")
-    # latest_title = get_latest_title()
-    # print(f"📌 Judul terbaru di database: '{latest_title}'")
-    # df = scrape_new_hoaxes(latest_title)
-    # df = scrape_all(df)
-    # df = retry_scrape_nan(df)
-    # df = clean_dataframe(df)
-    # print(df.columns)
+    print("🚀 Memulai proses scraping data hoaks...")
+    latest_title = get_latest_title()
+    print(f"📌 Judul terbaru di database: '{latest_title}'")
+    df = scrape_new_hoaxes(latest_title)
+    df = scrape_all(df)
+    df = retry_scrape_nan(df)
+    df = clean_dataframe(df)
+    print(df.columns)
     
-    # if df is None or df.empty:
-    #     print("✅ Tidak ada data hoaks baru untuk diproses hari ini.")
-    #     return None
+    if df is None or df.empty:
+        print("✅ Tidak ada data hoaks baru untuk diproses hari ini.")
+        return None
 
-    # list_id = insert_to_mysql(df)
-    # insert_to_chroma(df, list_id, model, collection, batch_size)
+    list_id = insert_to_mysql(df)
+    insert_to_chroma(df, list_id, model, collection, batch_size)
     
     token =data.get("token")
     url = "http://127.0.0.1:8000/scraper/done/" + token
